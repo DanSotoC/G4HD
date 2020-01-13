c5b5f2c28c51c6e14063fd34323ec9af02158b5f
Pablo <1715pablo@gmail.com>
2020-01-13 20:17:12 -0300

    cantidad pacientes y personal en dashboard dinamico

--- login/Hospital/hospital/dashboard/views.py
+++ login/Hospital/hospital/dashboard/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from usuarios.models import Paciente, Personal
 
 def home(request):
-	#pacientes = Usuario.objects.count()
+	pacientes=Paciente.objects.count()
+	personal=Personal.objects.count()
 	context = {
-
+			'pacientes':pacientes,
+			'personal':personal
 	}
 	return render(request,"dashboard.html", context)
