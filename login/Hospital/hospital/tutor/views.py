e762c99d348f50c47bf55dec8f28865bd72c4e84
RodrigoGarridoLeiva <rodrigogarridoleiva@gmail.com>
2020-01-14 14:36:53 -0300

    Se ajustan detalles de links del tutor

--- login/Hospital/hospital/tutor/views.py
+++ login/Hospital/hospital/tutor/views.py
@@ -6,9 +6,15 @@ from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 import os
 
+def logout_view(request):
+    logout(request)
+    return render(request,"main.html")
+
 def home_tutor(request):
 	return render(request,"home_tutor.html")
 
 def biblioteca_tutor(request):
 	archivo = Archivo.objects.all()
 	return render(request,'biblioteca_tutor.html',{'archivo':archivo})
+
+
