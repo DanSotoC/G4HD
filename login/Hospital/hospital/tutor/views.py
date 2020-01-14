4c70658641be47b3cea0b7960da4c1c8b8486304
RodrigoGarridoLeiva <rodrigogarridoleiva@gmail.com>
2020-01-14 16:19:09 -0300

    Se hace relacion de datos entre las tablas login, tutor y paciente para el perfil del tutor

--- login/Hospital/hospital/tutor/views.py
+++ login/Hospital/hospital/tutor/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from biblioteca.models import Archivo
+from usuarios.models import Paciente
+from usuarios.models import Tutor
 from django.views.generic import TemplateView ,View
- 
+
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 import os
@@ -11,7 +13,21 @@ def logout_view(request):
     return render(request,"main.html")
 
 def home_tutor(request):
-	return render(request,"home_tutor.html")
+	current_user = request.user
+	px = instance = get_object_or_404(Paciente, id_tutor_id = current_user.id)
+	tx = instance = get_object_or_404(Tutor, id_perfil_id = current_user.id)
+
+	context = {
+
+		"nom": current_user.first_name,
+		"ape":current_user.last_name,
+		"email": current_user.email,
+		"id_actual":current_user.id,
+		"paciente": px,
+		"tutor": tx,
+
+	}
+	return render(request,"home_tutor.html",context)
 
 def biblioteca_tutor(request):
 	archivo = Archivo.objects.all()
