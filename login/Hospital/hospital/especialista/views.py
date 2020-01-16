34bb911640ac726245607063b106e5f06df25abc
Pablo <1715pablo@gmail.com>
2020-01-16 17:22:31 -0300

    logout a especialista home especialista interactivo

--- login/Hospital/hospital/especialista/views.py
+++ login/Hospital/hospital/especialista/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import TemplateView
-
+from usuarios.models import Personal , Paciente, Perfil
 '''from .forms import PostForm'''
 from django.shortcuts import HttpResponse, HttpResponseRedirect, redirect
 from django.contrib import messages
 
 
+
+def logout_view(request):
+    logout(request)
+    return render(request,"main.html")
+
+
+
 def home_especialista(request):
-	return render(request,"index_especialista.html")
+	current_user = request.user
+	paciente=Paciente.objects.count()
+	px = instance = get_object_or_404(Personal, id_perfil_id = current_user.id)
+	perfil= Perfil.objects.get(id= current_user.id)
+	context = {
+
+		"nom": current_user.first_name,
+		"ape":current_user.last_name,
+		"email": current_user.email,
+		"perfil":perfil,
+		"id_actual":current_user.id,
+		"personal": px,
+		"num_pacientes":paciente
+		
+
+	}
+
+	return render(request,"index_especialista.html",context)
 
 def ausencia_paciente(request):
 	queryset =request.GET.get("buscar")
