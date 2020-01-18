5341ee33ba8ece8044643d78cc3103d79f0ccec7
Pablo <1715pablo@gmail.com>
2020-01-18 15:07:58 -0300

    listado de equipos/grupos creados (revisar envio de paquete al actualizar)

--- login/Hospital/hospital/equipos/views.py
+++ login/Hospital/hospital/equipos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import Group
+from usuarios.models import Personal
 
 # Create your views here.
 
@@ -10,18 +11,21 @@ def Crear_equipo_view(request):
 		nombre=request.POST.get('nombre_equipo')
 		group = Group.objects.create(name=nombre)
 		group.save()
-		return render(request,'listar_equipo.html')
+		return redirect(Listar_equipo_view)
 	else:
 		return render(request,'crear_equipo.html')
 	return render(request,'crear_equipo.html')
 
 def Listar_equipo_view(request):
 
-	return render(request,'listar_equipo.html')
+	group = Group.objects.all()
+
+	return render(request,'listar_equipo.html',{'group':group})
 
 
 
 
 def Asignar_equipo_view(request):
+	personal =Personal.objects.all()
 
-	return render(request,'asignar_equipo.html')
+	return render(request,'asignar_equipo.html',{'personal':personal})
