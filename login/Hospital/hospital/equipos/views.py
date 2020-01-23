aca178660f887120b314c37200f2c7e35c2560ee
Pablo <1715pablo@gmail.com>
2020-01-23 18:02:26 -0300

    ingreso y salida  de usuaro de equipo terminado/al borrar equipo usuarios vuelven a disponible

--- login/Hospital/hospital/equipos/views.py
+++ login/Hospital/hospital/equipos/views.py
@@ -28,25 +28,49 @@ def Asignar_equipo_view(request):
 	
 	personal=Personal.objects.all()
 	group = Group.objects.all()
+	user= User.objects.all()
 
 
-	return render(request,'asignar_equipo.html',{'personal':personal,'group':group})
+	return render(request,'asignar_equipo.html',{'personal':personal,'group':group,'user':user})
 
 
 
 def Ingreso_usuarios(request,id):
+	
+	group=Group.objects.get(id=id)
+	disp=Group.objects.get(name='Disponible')
+	if request.method=='POST':
+		if request.POST.getlist('id_usuario'):
+			id_user=request.POST.getlist('id_usuario')
+			for i in id_user:
+				user=User.objects.get(id=i)
+				group.user_set.add(user)
+				disp.user_set.remove(user)
+		else:
+			if request.POST.getlist('id_usuario_s'):
+				id_user=request.POST.getlist('id_usuario_s')
+				for i in id_user:
+					user=User.objects.get(id=i)
+					disp.user_set.add(user)
+					group.user_set.remove(user)	
+
 	disponible=User.objects.filter(groups__name='Disponible')
+	equipo=User.objects.filter(groups__id=id)
 	personal=Personal.objects.all()
 
 
 
-	return render(request,"ingreso_usuarios_grupo.html",{'disponible':disponible,'personal':personal})
+	return render(request,"ingreso_usuarios_grupo.html",{'disponible':disponible,'personal':personal,'equipo':equipo,'group':group})
 
 
 
 def Eliminar_equipo(request,id):
 	group=Group.objects.get(id=id)
+	dispgroup=Group.objects.get(name='Disponible')
 	if request.method=='POST':
+		disp=User.objects.filter(groups__id=id)
+		for d in disp:
+			dispgroup.user_set.add(d)
 		group.delete()
 		return redirect(Listar_equipo_view)
 	return render(request,"eliminar_equipo.html",{'group':group})
