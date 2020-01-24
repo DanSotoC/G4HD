d4a170c6c2fef421ce7a965baa200cae7af3cb21
Pablo <1715pablo@gmail.com>
2020-01-23 21:14:37 -0300

    problema guardado en bd hora_visita solucionado

--- login/Hospital/hospital/visita/views.py
+++ login/Hospital/hospital/visita/views.py
@@ -1,20 +1,24 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404,redirect
 from usuarios.models import Paciente
 from .forms import  Agendar
+from lista.views import usuarios_listpa
 
 def agendar_visita(request, id=None):
-	aux = get_object_or_404(Paciente, id = id)	
-	form = Agendar(request.POST or None)
+	aux = Paciente.objects.get(id=id)	
+	form=Agendar(request.POST)
+	if request.method=='POST':
+		
+		
+		
 
-	if form.is_valid():
-		instance=form.save(commit=False)
-		instance.save()
+		if form.is_valid():
+			form.save()
+
 			
-	context = {	
+		return redirect(usuarios_listpa)
+	else:
+		form = Agendar()
+	return render(request,"agendar_visita.html",{"form":form,"px":aux})
 
-		"px": aux,
-		"form":form,
-	}
-	return render(request,"agendar_visita.html",context)
 	
 
