e01fc0f3c1100c1c11beda877045df2fdbb6a8fc
Pablo <1715pablo@gmail.com>
2020-02-16 00:09:26 -0300

    arreglo cosas de diseño

--- login/Hospital/hospital/usuarios/admin.py
+++ login/Hospital/hospital/usuarios/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin 
-from .models import Paciente
+from .models import Paciente ,Personal, Tutor
 
 admin.site.register(Paciente)
+admin.site.register(Personal)
+admin.site.register(Tutor)
+
